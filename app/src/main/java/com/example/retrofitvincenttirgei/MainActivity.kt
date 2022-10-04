e8bc571ffb426d5ab0106dcce17ee2d95a25fc3b
Vlad <ricean.vlad7@gmail.com>
2022-10-04 08:18:59 +0300

    -init

---
+++ app/src/main/java/com/example/retrofitvincenttirgei/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.retrofitvincenttirgei
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Article
+ * https://medium.com/android-news/token-authorization-with-retrofit-android-oauth-2-0-747995c79720
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
