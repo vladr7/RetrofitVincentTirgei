29c137c622f55b49dc446a57843894a5c4557ccf
Vlad <ricean.vlad7@gmail.com>
2022-10-04 15:05:06 +0300

    -added dependencies for kotlinx serialization

--- app/src/main/java/com/example/retrofitvincenttirgei/Constants.kt
+++ app/src/main/java/com/example/retrofitvincenttirgei/Constants.kt
@@ -6,7 +6,6 @@ object Constants {
     // Endpoints
     const val BASE_URL = "https://reqres.in/api/"
     const val LOGIN_URL = "login"
-    const val POSTS_URL = "posts"
     const val USERS_URL = "users"
     const val RESOURCE_URL = "unknown"
 
